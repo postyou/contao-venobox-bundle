b17772a0706d6a0294e2676b1a8d5bd41498af0a
MarkejN <markus.nestmann@gmx.de>
2020-08-03 14:39:05 +0200

    restructure classes

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -2,21 +2,19 @@
 
 namespace Postyou\ContaoVenoboxBundle\ContaoManager;
 
-use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
-use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Contao\CoreBundle\ContaoCoreBundle;
 use Postyou\ContaoVenoboxBundle\PostyouContaoVenoboxBundle;
 
 class Plugin implements BundlePluginInterface
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function getBundles(ParserInterface $parser)
+    public function getBundles(ParserInterface $parser): array
     {
         return [
-            BundleConfig::create(ContaoVenoboxBundle::class)->setLoadAfter([ContaoCoreBundle::class]),
+            BundleConfig::create(PostyouContaoVenoboxBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class]),
         ];
     }
 }
